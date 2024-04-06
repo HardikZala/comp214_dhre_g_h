b9b44695802655902ec87f5e8e0640b50a4562f7
HardikZala <hz6314@gmail.com>
2024-04-06 11:52:30 -0400

    Your commit message here

--- dream_house_g_h/app_menu/urls.py
+++ dream_house_g_h/app_menu/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'),
-    path('list_staff', views.list_staff, name='list_staff')
+    path('list_staff', views.list_staff, name='list_staff'),
+    path('staff_menu', views.staff_menu, name ='staff_menu'),
+    path('branch_menu', views.branch_menu, name ='branch_menu'),
+    path('client_menu', views.client_menu, name ='client_menu'),
+    path('update_staff', views.update_staff, name='update_staff'),
 ]
--- dream_house_g_h/app_menu/views.py
+++ dream_house_g_h/app_menu/views.py
@@ -1,18 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Staff
+from django.db import connection
+
 
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'menus': [
+            {'name': 'Staff', 'url': 'staff_menu'},
+            {'name': 'Branch', 'url': 'branch_menu'},
+            {'name': 'Client', 'url': 'client_menu'}
+        ]
+    }
+    return render(request, 'index.html', context)
+
+
+from django.shortcuts import render
 
 def list_staff(request):
     staffs = Staff.objects.all().values()
-    template = loader.get_template('list_staff.html')
-    context={
+    context = {
         'staffs': staffs
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'list_staff.html', context)
+
+
+def update_staff(request):
+    if request.method == 'POST':
+        staffno = request.POST.get('staffno')
+        salary = request.POST.get('salary')
+        telephone = request.POST.get('telephone')
+        email = request.POST.get('email')
+
+        with connection.cursor() as cursor:
+            cursor.callproc('update_staff_sp', [staffno, salary, telephone, email])
+
+        return redirect('list_staff')
+
+def staff_menu(request):
+    template = loader.get_template('staff_menu.html')
+    return HttpResponse(template.render())
+
+def branch_menu(request):
+    template = loader.get_template('branch_menu.html')
+    return HttpResponse(template.render())
+
+def client_menu(request):
+    template = loader.get_template('client_menu.html')
+    return HttpResponse(template.render())
+
+from django.db import connection
+
+def test_db_connection():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM DH_STAFF FETCH FIRST 1 ROWS ONLY")
+        row = cursor.fetchone()
+    return row
